3e22ed0ee2fb428e0095c3774ac4e5b3ecbdddc7
sakul-the-one <68478969+sakul-the-one@users.noreply.github.com>
2021-11-28 12:49:33 +0100

    Create SaveScript1.cs

---
+++ SaveScript Ep1/SaveScript1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LGCON;
+using System.Text;
+
+public class SaveScript1 : MonoBehaviour
+{
+    private void Start()
+    {
+        //FileStream fs = new FileStream("D:\\t.txt", FileMode.Create);
+        //StreamWriter SW = new StreamWriter(fs);
+        //SW.WriteLine("Hi: Hello There!\nTT: Hi, this is a Message!\n");
+        //SW.Close();
+        string path = "D:\\t.txt";
+        string[] mes =
+        {
+        "Name: Sakul",
+        "Age: 1001",
+        "Health: 2001",
+        "School: Boring"
+        };
+
+        SaveScript t = new SaveScript();
+        t.path = path;
+        t.Write(mes);
+        t.Read();
+        t.Log();
+        Debug.Log("............................................");
+        Debug.Log(t.GetVariable("Name"));
+        Debug.Log(t.GetVariable("TT"));
+    }
+}
