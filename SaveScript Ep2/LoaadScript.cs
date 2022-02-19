67caadd6e4e36b748809ab61063c1b56a79a61d1
sakul-the-one <68478969+sakul-the-one@users.noreply.github.com>
2022-02-19 10:38:20 +0100

    Update LoaadScript.cs

--- SaveScript Ep2/LoaadScript.cs
+++ SaveScript Ep2/LoaadScript.cs
@@ -5,15 +5,16 @@ using LGCON;
 
 public class LoaadScript : MonoBehaviour
 {
-    public void Save(int value) 
+    public void Save(int value)
     {
-        SaveScript ss = new SaveScript("D:\\T.LGCon");
+        SaveScript saver = new SaveScript("D:\\T.LGCon");
+
         string[] mes =
             {
         "x:"+transform.position.x,
         "y:"+transform.position.y,
         "z:"+transform.position.z
         };
-        ss.Write("T"+value, mes, true);
+        saver.Write("T" + value, mes, true);
     }
 }
